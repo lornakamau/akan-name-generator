82cc25235cfe5af0d49add84dbdfa922062eb8c8
Lorna Kamau <kamaulorna@gmail.com>
2020-03-28 01:30:35 +0300

    add comments to js file

--- js/akan.js
+++ js/akan.js
@@ -2,153 +2,62 @@ document.getElementById("submit").addEventListener("click",function(event){
     event.preventDefault()
   })
   
-  //get input from HTML
-  function readData(){
-  var dd = document.getElementById("dd").value;
-  var mm = document.getElementById("mm").value;
-  var yy = document.getElementById("yy").value;
-  
-  
-  if (document.getElementById("male").checked){
-    var male = true;
-  }else{
-    var female = true;
-  }
-  //calculation of day of the week
-  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  var maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"];
-  var femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  var birthday = new Date(mm + '/' + dd + '/' + yy);
-  var dayOfTheWeek = birthday.getDay();
-  
-
-if (dd <= 0 || dd >= 31){
-    document.getElementById("feedback").innerText = "Please enter a valid date!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "36%"
-  }
-  else if(mm === "------"){
-    document.getElementById("feedback").innerText = "Please select a month!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "40%"
-  }
-  else if(yy <= 0 || yy> 2020) {
-    document.getElementById("feedback").innerText = "Please enter a valid year!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "38%"
-  }
-  else if( (mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4) ){
-      document.getElementById("feedback").innerText = "Please enter a valid date!"
-      document.getElementById("feedback").style.color = "red"
-      document.getElementById("feedback").style.paddingLeft = "38%"
-  }
-  else if( (mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4) ){   
-      document.getElementById("feedback").innerText = "Please enter a valid date!"
-      document.getElementById("feedback").style.color = "red"
-      document.getElementById("feedback").style.paddingLeft = "38%"
-  }
-  else if (male === true) {
-    document.getElementById("display").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" +  "Your Akan name is " + maleNames[dayOfTheWeek] + "!"
-    document.getElementById("display").style.color = "MediumBlue"
-    document.getElementById("display").style.padding = "9% 22% 13% 22%"
-    document.getElementById("display").style.fontSize = "35px"
-    document.getElementById("display").style.fontFamily = "'Kalam', cursive"
-    document.getElementById("display").style.textAlign = "center"
+function readData(){ 
+    var dd = document.getElementById("dd").value; //get date input 
+    var mm = document.getElementById("mm").value; //get month input 
+    var yy = document.getElementById("yy").value; //get year input 
+    
+    if (document.getElementById("male").checked){ //get input from radio
+      var male = true;
+    }else{
+      var female = true;
     }
 
-    else if (female === true) { 
-    document.getElementById("display").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!"
-    document.getElementById("display").style.color = "DeepPink"
-    document.getElementById("display").style.padding = "9% 22% 13% 22%"
-    document.getElementById("display").style.fontSize = "35px"
-    document.getElementById("display").style.fontFamily = "'Kalam', cursive"
-    document.getElementById("display").style.textAlign = "center"
-    }
-  }
-/*
-  if(yy< 2020) {
-    if(mm === "------"){
-      document.getElementById("feedback").innerText = "Please select a month!"
-      document.getElementById("feedback").style.color = "red"
-      document.getElementById("feedback").style.paddingLeft = "40%"
-    }
-    if( (mm === "February") && (0 == yy % 4) && (0 != yy % 100) || (0 == yy % 400) ){
-        if (dd <= 0 || dd > 29){
-          document.getElementById("feedback").innerText = "Please enter a valid date!"
-          document.getElementById("feedback").style.color = "red"
-          document.getElementById("feedback").style.paddingLeft = "38%"
-          }
-    }
-    if( (mm === "February") && (0 != yy % 4) ){   
-        if (dd <= 0 || dd > 28){
-          document.getElementById("feedback").innerText = "Please enter a valid date!"
-          document.getElementById("feedback").style.color = "red"
-          document.getElementById("feedback").style.paddingLeft = "38%"
-          }
-    }
-    if ((mm != "February") && (dd <= 0 || dd >= 31)){
-      document.getElementById("feedback").innerText = "Please enter a valid date!"
-      document.getElementById("feedback").style.color = "red"
-      document.getElementById("feedback").style.paddingLeft = "36%"
-    }
-  }
-  else{
-    document.getElementById("feedback").innerText = "Please enter a valid year!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "38%"
-  }
-}
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; //array for days
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"]; //array for male Akan names
+    var femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //array for female Akan names
+    var birthDate = new Date(mm + '/' + dd + '/' + yy);
+    var dayOfTheWeek = birthDate.getDay(); //calculation of day of the week
 
-      /*
-  //Validitation and allocation of names
-    if (mm != "February"){
-      if (dd <= 0 || dd >= 31){
-    document.getElementById("feedback").innerText = "Please enter a valid date!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "36%"
-    } 
-
-    else if(mm === "------"){
-    document.getElementById("feedback").innerText = "Please select a month!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "40%"
-    } 
-
-    else if(mm === "February"){
-      if( (0 == yy % 4) && (0 != yy % 100) || (0 == yy % 400) ){
-        if (dd <= 0 || dd > 29){
-      document.getElementById("feedback").innerText = "Please enter a valid year!"
-      document.getElementById("feedback").style.color = "red"
-      document.getElementById("feedback").style.paddingLeft = "38%"
+    if (dd <= 0 || dd >= 31){ //ensures user enters a date and validates date entered
+        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error 
+        document.getElementById("error").style.color = "red"
+        document.getElementById("error").style.paddingLeft = "36%"
       }
-      else {
-        document.getElementById("feedback").innerText = "Please enter a valid year!"
-      document.getElementById("feedback").style.color = "red"
-      document.getElementById("feedback").style.paddingLeft = "38%"
+    else if(mm === "------"){ //ensures user selects a month
+        document.getElementById("error").innerText = "Please select a month!" //shows error message by replacing the HTML content of id=error
+        document.getElementById("error").style.color = "red"
+        document.getElementById("error").style.paddingLeft = "40%"
       }
-    }
-
-    else if(yy <= 0 || yy> 2020) {
-    document.getElementById("feedback").innerText = "Please enter a valid year!"
-    document.getElementById("feedback").style.color = "red"
-    document.getElementById("feedback").style.paddingLeft = "38%"
-    }
-    else if (male === true) {
-    document.getElementById("display").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" +  "Your Akan name is " + maleNames[dayOfTheWeek] + "!"
-    document.getElementById("display").style.color = "MediumBlue"
-    document.getElementById("display").style.padding = "9% 22% 13% 22%"
-    document.getElementById("display").style.fontSize = "35px"
-    document.getElementById("display").style.fontFamily = "'Kalam', cursive"
-    document.getElementById("display").style.textAlign = "center"
-    }
-
-    else if (female === true) { 
-    document.getElementById("display").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!"
-    document.getElementById("display").style.color = "DeepPink"
-    document.getElementById("display").style.padding = "9% 22% 13% 22%"
-    document.getElementById("display").style.fontSize = "35px"
-    document.getElementById("display").style.fontFamily = "'Kalam', cursive"
-    document.getElementById("display").style.textAlign = "center"
-    }
+    else if(yy <= 0 || yy > 2020) { //ensures user enters a year and the year entered is valid and doesn't exceed 2020
+        document.getElementById("error").innerText = "Please enter a valid year!" //shows error message by replacing the HTML content of id=error
+        document.getElementById("error").style.color = "red"
+        document.getElementById("error").style.paddingLeft = "38%"
+      }
+    else if( (mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4) ){ //sets limit for valid date for the month of February in a leap year to 29
+        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error
+        document.getElementById("error").style.color = "red"
+        document.getElementById("error").style.paddingLeft = "38%"
+      }
+    else if( (mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4) ){ //sets limit for valid date for the month of February in a non-leap year to 28 
+        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error
+        document.getElementById("error").style.color = "red"
+        document.getElementById("error").style.paddingLeft = "38%"
+      }
+    else if (male === true) { //styling of results for a male user
+        document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" +  "Your Akan name is " + maleNames[dayOfTheWeek] + "!" //shows result by replacing the  HTML content in the id=result
+        document.getElementById("result").style.color = "MediumBlue"
+        document.getElementById("result").style.padding = "9% 22% 13% 22%"
+        document.getElementById("result").style.fontSize = "35px"
+        document.getElementById("result").style.fontFamily = "'Kalam', cursive"
+        document.getElementById("result").style.textAlign = "center"
+      }
+    else if (female === true) { //styling results of a female user
+        document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!" //shows result by replacing the HTML content in the id=result
+        document.getElementById("result").style.color = "DeepPink"
+        document.getElementById("result").style.padding = "9% 22% 13% 22%"
+        document.getElementById("result").style.fontSize = "35px"
+        document.getElementById("result").style.fontFamily = "'Kalam', cursive"
+        document.getElementById("result").style.textAlign = "center"
+      }
   }
-} */
